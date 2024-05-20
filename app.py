49077ad06ee2235a3c5f3e299f8ef1a6b5ea4d7a
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2024-05-20 10:23:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pickle
-
+from sklearn.preprocessing import LabelEncoder
 # Load your saved model
 with open('Ferment_linear_model.pkl', 'rb') as f:
     model = pickle.load(f)
@@ -13,18 +13,22 @@ def predict(input_data):
     return prediction
 
 # Streamlit UI
+# Streamlit UI
 def main():
     st.title('Machine Learning Model Predictor')
 
     # Input features
-    feature1 = st.number_input('WET BIOMASS(g)', min_value=0, max_value=100)
-    feature2 = st.number_input('DRY BIOMASS(g)', min_value=0, max_value=100)
-    feature3 = st.text_input('CARBON SOURCE')
+    feature1 = st.number_input('WET BIOMASS(g)', min_value=0, max_value=100, step=1)
+    feature2 = st.number_input('DRY BIOMASS(g)', min_value=0, max_value=100, step=1)
+    feature3 = st.text_input('CARBON SOURCE')  # Assuming CARBON SOURCE is a text input
 
+    
+    labelencode=LabelEncoder()
+df['feature3']=labelencode.fit_transform(df['feature3'])
     # Button to trigger prediction
     if st.button('Predict'):
         # Make prediction
-        input_data = [[feature1, feature2,feature3]]  # Adjust according to your model's input format
+        input_data = [[feature1, feature2, feature3]]  # Adjust according to your model's input format
         prediction = predict(input_data)
 
         # Display prediction result
