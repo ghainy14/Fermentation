90e9abe6f0ab1e1f9446d96935a8fab8493a4fa9
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2024-05-27 11:29:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def main():
     st.title('Machine Learning Model Predictor')
 
     # Input features
-    feature1 = st.number_input('WET BIOMASS(g)', min_value=0, max_value=100, step=1)
-    feature2 = st.number_input('DRY BIOMASS(g)', min_value=0, max_value=100, step=1)
+    feature1 = st.number_input('WET BIOMASS(g)', min_value=0, max_value=100, step=0.1)
+    feature2 = st.number_input('DRY BIOMASS(g)', min_value=0, max_value=100, step=0.1)
 
     # Dropdown for CARBON SOURCE
     carbon_sources = ['Shea butter kernel extract', 'Ipomoea Batatas Peel ectract', 'Palm Fruit empty fibre']  # Example options
